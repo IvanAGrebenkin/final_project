f996a76d7532a14c9e5f76ad53050e5431b81aa3
Admin <ivanagrebenkin@gmail.com>
2022-03-08 21:06:13 +0700

    Добавлена кнопка сброса на экране счетчика

--- lib/Screens/counter.dart
+++ lib/Screens/counter.dart
@@ -35,6 +35,15 @@ class _CounterState extends State<Counter> {
     });
   }
 
+  void _counterReset() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = 0;
+      prefs.setInt('counter', _counter);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -47,26 +56,37 @@ class _CounterState extends State<Counter> {
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
           onPressed: _incrementCounter,
-          // onPressed: _incrementCounter,
           tooltip: 'Нажатие увеличивает значение на единицу',
           child: const Icon(Icons.add),),
         body: Container (
           padding: const EdgeInsets.symmetric(horizontal: 10),
-
-            child: Center(
-              child: Column(mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text('Количество нажатий кнопки:'),
-                  Text(
-                    // 'Счетчик',
-                    '$_counter',
-                    style: Theme.of(context).textTheme.headline4,
+          child: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Количество нажатий кнопки:'),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headline4,
+                ),
+                SizedBox(width:154, height:42,
+                  child: ElevatedButton(
+                    onPressed: _counterReset,
+                    child: const Text('Сброс счетчика',
+                      style: TextStyle(
+                        fontSize:16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.green,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(36.0)),
+                    ),
                   ),
-                ],),
-
+                ),// Кнопка "Заказать"
+              ],
             ),
-
-
+          ),
         ),
         appBar: appBar(context),
         drawer: navDrawer(context),
